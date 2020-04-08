657304675a57eb383e5e5439ed38758d7973545d
cswang <cswang07@foxmail.com>
2020-04-08 15:17:50 +0800

    Init Nlog

--- ChuXin.EMIS.WebAPI/Controllers/StudentsController.cs
+++ ChuXin.EMIS.WebAPI/Controllers/StudentsController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using ChuXin.EMIS.WebAPI.IServices;
 using ChuXin.EMIS.WebAPI.SettingModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace ChuXin.EMIS.WebAPI.Controllers
@@ -13,17 +14,21 @@ namespace ChuXin.EMIS.WebAPI.Controllers
     {
         private readonly IOptions<AppSetting> _appOptions;
         private readonly IStudentRepository _studentRepository;
-        public StudentsController(IOptions<AppSetting> appOptions, IStudentRepository studentRepository)
+        private readonly ILogger<StudentsController> _logger;
+        public StudentsController(IOptions<AppSetting> appOptions, IStudentRepository studentRepository, ILogger<StudentsController> logger)
         {
             _appOptions = appOptions;
             _studentRepository = studentRepository;
+            _logger = logger;
+
+            _logger.LogDebug(1, "NLog injected into HomeController");
         }
 
         [HttpGet]
         public async Task<IActionResult> GetStudents()
         {
             var studentList = await _studentRepository.GetStudentsAsync();
-
+            _logger.LogInformation("test");
             return Ok(studentList);
         }
     }
--- ChuXin.EMIS.WebAPI/Program.cs
+++ ChuXin.EMIS.WebAPI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace ChuXin.EMIS.WebAPI
 {
@@ -13,7 +14,21 @@ namespace ChuXin.EMIS.WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("--------------Init Main------------------");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +36,12 @@ namespace ChuXin.EMIS.WebAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(logging => {
+                    logging.ClearProviders();
+                    // 设置日志最低等级  Trace -> Debug -> Info -> Warning -> Error -> Fatal
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+                })
+                .UseNLog(); // 注入
     }
 }
