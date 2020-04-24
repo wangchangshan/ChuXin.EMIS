bfd84d65fcdc09849db78e77eefd83cf00139834
cswang <cswang07@foxmail.com>
2020-04-24 15:47:15 +0800

    enhance

--- ChuXin.EMIS.WebAPI/Enums/LineFlagEnum.cs
+++ ChuXin.EMIS.WebAPI/Enums/LineFlagEnum.cs
@@ -3,6 +3,7 @@
 	public enum LineFlagEnum
 	{
 		正常数据 = 1,
+		测试数据 = 2,
 		已删除数据 = 9
 	}
 }
--- ChuXin.EMIS.WebAPI/SettingModel/EMISSetting.cs
+++ ChuXin.EMIS.WebAPI/SettingModel/EMISSetting.cs
@@ -1,12 +1,21 @@
 ﻿namespace ChuXin.EMIS.WebAPI.SettingModel
 {
-    public class EMISSetting
-    {
-        public string AccessUrl { get; set; }
-        public string UserExpireTime { get; set; }
-        public int ImageCompressLevel { get; set; }
-        public int WeiXinImageWidth { get; set; }
-        public string StatisticsStartDate { get; set; }
-        public string Urls { get; set; }
-    }
+	public class EMISSetting
+	{
+		public string AccessUrl { get; set; }
+
+		public string UserExpireTime { get; set; }
+
+		public int ImageCompressLevel { get; set; }
+
+		public int WeiXinImageWidth { get; set; }
+
+		public string StatisticsStartDate { get; set; }
+
+		public string Urls { get; set; }
+
+		public string WeChatAppId { get; set; }
+
+		public string WeChatAppSecret { get; set; }
+	}
 }
